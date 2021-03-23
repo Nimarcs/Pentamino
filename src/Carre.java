43a30b5b3d240b3e9f316499234cee16744008b5
Nimarcs <moimonautremoi@gmail.com>
2021-03-23 11:06:17 +0100

    ajout de Carre sans methode

---
+++ src/Carre.java
@@ -0,0 +1,57 @@
+public class Carre {
+
+    //attributs
+
+    /**
+     * coordonnee de la piece
+     */
+    private int x, y;
+
+    //Contructeurs
+
+    /**
+     * Contructeur de Carre
+     * @param pX abcsisse du carre
+     * @param pY ordonnee du carre
+     */
+    public Carre(int pX, int pY){
+        this.x = pX;
+        this.y = pY;
+    }
+
+    //getters et setters
+
+    /**
+     * getter de x
+     * @return x
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * getter de y
+     * @return y
+     */
+    public int getY() {
+        return y;
+    }
+
+    /**
+     * setter de x
+     * @param x nouvelle valeur de x
+     */
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    /**
+     * setter de y
+     * @param y nouvelle valeur de y
+     */
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    //methodes
+}
