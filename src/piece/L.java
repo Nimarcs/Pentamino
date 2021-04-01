2d181b5b6c674ba43e3bdc1f059f579327f000c3
RICHIER Marcus <marcus.richier8@etu.univ-lorraine.fr>
2021-04-01 20:04:17 +0200

    Correction de la gestion d'erreur des pieces

--- src/piece/L.java
+++ src/piece/L.java
@@ -1,8 +1,14 @@
 package piece;
 
+import exceptions.CharInvalide;
+import exceptions.CoordonneeInvalide;
+import exceptions.ValeurNonTraite;
+
+import java.io.IOException;
+
 public class L extends Piece{
 
-    public L(int x, int y) throws Exception {
+    public L(int x, int y) throws CoordonneeInvalide, ValeurNonTraite, CharInvalide, IOException {
         super(x, y, 'L', "L.txt");
     }
 
