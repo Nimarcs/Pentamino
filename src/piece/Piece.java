dd301fef27f397c7a046d3beffca23a853a69349
BERGERAT Hugo <hugo.bergerat2002@gmail.com>
2021-04-02 21:21:11 +0200

    Ajout de la méthode toString dans Piece

--- src/piece/Piece.java
+++ src/piece/Piece.java
@@ -118,6 +118,14 @@ public class Piece {
     }
 
     @Override
+    public String toString() {
+        return "Piece{" +
+                "  lettre=" + lettre +
+                ", carres=" + carres +
+                '}';
+    }
+
+    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
