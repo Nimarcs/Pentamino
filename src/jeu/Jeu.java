4d38769f6f51258227d7bb7c388d1b8a86fee657
RICHIER Marcus <marcus.richier8@etu.univ-lorraine.fr>
2021-04-03 16:34:28 +0200

    Mise en forme de Jeu
    Ajout de javadoc manquante
    ajout de commentaire
    Aeration du code

--- src/jeu/Jeu.java
+++ src/jeu/Jeu.java
@@ -10,21 +10,51 @@ import java.util.Scanner;
 
 public class Jeu {
 
+    //attributs
+
+    /**
+     * liste des joueurs
+     */
     private List<Joueur> joueurList;
+
+    /**
+     * partie en cours
+     */
     private Partie partie;
+
+    /**
+     * signale la fin d'une partie
+     */
     private boolean estFinis;
 
+    /**
+     * liste des commandes disponible pour jouer
+     */
     private List<Commande> commandes;
     private static Scanner scanner = new Scanner(System.in);
 
+
+    //constructeur
+
+    /**
+     * constructeur vide de Jeu
+     */
     public Jeu() {
         this.estFinis = false;
     }
 
+    //methodes
+
+    /**
+     * methode permettant d'arreter le jeu
+     */
     public void finirJeu() {
         this.estFinis = true;
     }
 
+    /**
+     * methode permettant de redefinir les commandes disponible
+     */
     private void enregistrerCommandes() {
         this.commandes = new ArrayList<Commande>();
         commandes.add(new AideCommande(this.commandes));
@@ -34,6 +64,10 @@ public class Jeu {
         commandes.add(new AfficherGrilleCommande(this.partie));
     }
 
+    /**
+     * methode permettant d'executer une commande
+     * @param commandeStr nom de la commande
+     */
     private void executerCommande(String commandeStr) {
         String[] commandeDonnee = commandeStr.split(" ");
         for (Commande commande : this.commandes) {
@@ -41,24 +75,46 @@ public class Jeu {
         }
     }
 
+    /**
+     * methode permettant de lancer le jeu
+     * @throws DimensionsInvalide renvoye par Partie lorsque les dimentions de la grille sont invalides
+     * @throws CharInvalide renvoye par Partie lorsque qu'un caractere est invalide
+     * @throws CoordonneeInvalide renvoye par Partie lorsque des coordonee sont invalides
+     * @throws IOException renvoye par Partie lors d'une erreur lors de la lecture d'un fichier
+     * @throws ValeurNonTraite renvoye par Partie lors d'une erreur lorsqu'un caratere non traite est trouve dans un fichier
+     */
     public void lancerJeu() throws DimensionsInvalide, CharInvalide, CoordonneeInvalide, IOException, ValeurNonTraite {
-        //boucle jusqu'a la fin des tours
+
+        //creer un scanner
+        Scanner scanner = new Scanner(System.in);
+
+        //initialisation des valeurs necessaire
         this.partie = new Partie(10, 10);
         this.enregistrerCommandes();
+
+        //TODO ajout des joueurs
+
+        //boucle jusqu'a la fin des tours
         while (!this.estFinis) {
             System.out.println("Entrez une commande (taper \"aide\" pour une liste de commandes)");
             String commande = scanner.nextLine();
             this.executerCommande(commande);
+
         }
     }
 
+    //main
+
     public static void main(String[] args) {
+
+        //on creer puis lance un jeu
         Jeu jeu = new Jeu();
         try {
             jeu.lancerJeu();
-        } catch (DimensionsInvalide | CharInvalide | CoordonneeInvalide | IOException | ValeurNonTraite dimensionsInvalide) {
-            dimensionsInvalide.printStackTrace();
+        } catch (DimensionsInvalide | CharInvalide | CoordonneeInvalide | IOException | ValeurNonTraite erreur) {
+            erreur.printStackTrace();
         }
+        
     }
 
 }
