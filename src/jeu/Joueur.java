2e5e83875bfab05a2bf269746f5efa763e47ca37
RICHIER Marcus <marcus.richier8@etu.univ-lorraine.fr>
2021-04-05 11:10:33 +0200

    Debut de joueur
    La base de joueur

--- src/jeu/Joueur.java
+++ src/jeu/Joueur.java
@@ -1,9 +1,60 @@
 package jeu;
 
-public class Joueur {
+public abstract class Joueur {
+
+    //attributs
 
     private Partie[] parties;
 
     private int score;
+
+    //contructeurs
+    public Joueur(){
+        this.parties = new Partie[0];
+        this.score = 0;
+    }
+
+    //getter et setter
+
+    public int getScore() {
+        return this.score;
+    }
+
+    public Partie[] getParties() {
+        return this.parties;
+    }
+
+    public Partie getPartie(int index){
+        if (index < this.parties.length && index > 0){
+            return this.parties[index];
+        } else{
+            return null;
+        }
+    }
+
+    public Partie getLastPartie(){
+        if (this.parties.length == 0)
+            return null;
+        return this.parties[this.parties.length -1];
+    }
+
+    //methodes
+
+    public void ajouterPartie(Partie partie){
+        Partie[] temp = new Partie[this.parties.length +1];
+
+        for (int i = 0; i < this.parties.length; i++){
+            temp[i] = this.parties[i];
+        }
+
+        //TODO que faire si partie est null ?
+        temp[temp.length -1] = partie;
+
+        this.parties = temp;
+
+    }
+
+    public abstract void calculerScore();
+
 
 }
