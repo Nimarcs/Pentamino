6e099a330a0a82992ec2951347a23836c6ed5701
RICHIER Marcus <marcus.richier8@etu.univ-lorraine.fr>
2021-04-05 12:01:56 +0200

    Ajout de JoueurIntermediaire

--- src/jeu/JoueurIntermediaire.java
+++ src/jeu/JoueurIntermediaire.java
@@ -1,4 +1,42 @@
 package jeu;
 
-public class JoueurIntermediaire {
+import exceptions.*;
+
+import java.util.List;
+
+public class JoueurIntermediaire extends Joueur {
+
+    //constructeurs
+
+    public JoueurIntermediaire(){
+        super();
+    }
+
+    //methodes
+
+    public void calculerScore() {
+        double score = 0;
+        List<Partie> parties= this.getParties();
+        for (int i = 0; i < parties.size(); i++){
+            score += parties.get(i).getScore() * 0.5;
+        }
+        this.setScore(score);
+    }
+
+    public void ajouterPiece(int num, int x, int y) {
+        try {
+            this.getLastPartie().ajouterPiece(num, x, y);
+        } catch (PieceDebordeTerrain pieceDebordeTerrain) {
+            //autorise
+            pieceDebordeTerrain.printStackTrace();
+        } catch (CaseDejaOccupe caseDejaOccupe) {
+            caseDejaOccupe.printStackTrace();
+        } catch (NumeroInconnue numeroInconnue) {
+            numeroInconnue.printStackTrace();
+        } catch (PartieInconnue partieInconnue) {
+            partieInconnue.printStackTrace();
+        } catch (CoordonneeInvalide coordonneeInvalide) {
+            coordonneeInvalide.printStackTrace();
+        }
+    }
 }
