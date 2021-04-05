323eaaa5a584ac3c105fc0482c763752c74c9a74
RICHIER Marcus <marcus.richier8@etu.univ-lorraine.fr>
2021-04-05 12:05:12 +0200

    Ajout de JoueurAvance
    et fix de la typo dans son nom

---
+++ src/jeu/JoueurAvance.java
@@ -0,0 +1,42 @@
+package jeu;
+
+import exceptions.*;
+
+import java.util.List;
+
+public class JoueurAvance extends Joueur{
+
+    //constructeurs
+
+    public JoueurAvance(){
+        super();
+    }
+
+    //methodes
+
+    public void calculerScore() {
+        double score = 0;
+        List<Partie> parties= this.getParties();
+        for (int i = 0; i < parties.size(); i++){
+            score += parties.get(i).getScore() * 1; 
+        }
+        this.setScore(score);
+    }
+
+
+    public void ajouterPiece(int num, int x, int y) {
+        try {
+            this.getLastPartie().ajouterPiece(num, x, y);
+        } catch (PieceDebordeTerrain pieceDebordeTerrain) {
+            pieceDebordeTerrain.printStackTrace();
+        } catch (CaseDejaOccupe caseDejaOccupe) {
+            caseDejaOccupe.printStackTrace();
+        } catch (NumeroInconnue numeroInconnue) {
+            numeroInconnue.printStackTrace();
+        } catch (PartieInconnue partieInconnue) {
+            partieInconnue.printStackTrace();
+        } catch (CoordonneeInvalide coordonneeInvalide) {
+            coordonneeInvalide.printStackTrace();
+        }
+    }
+}
