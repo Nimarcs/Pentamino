51ab599af990e5332f7a65eb126165929494fed4
BERGERAT Hugo <hugo.bergerat2002@gmail.com>
2021-04-05 18:30:17 +0200

    création de la commande d'aide pur le jeu

---
+++ src/commandes/jeu/AideJeuCommande.java
@@ -0,0 +1,28 @@
+package commandes.jeu;
+
+import commandes.Commande;
+import jeu.Jeu;
+
+import java.util.List;
+
+public class AideJeuCommande extends CommandeJeu {
+
+    private List<CommandeJeu> commandes;
+
+    public AideJeuCommande(List<CommandeJeu> commandes) {
+        super("aide");
+        this.commandes = commandes;
+    }
+
+    public void afficherAide() {
+        System.out.println(" -aide: Affiche toutes les commandes.");
+    }
+
+    public void executer(String[] args, Jeu jeu) {
+        System.out.println("Liste de toutes les commandes:");
+        for (Commande commande : this.commandes) {
+            commande.afficherAide();
+        }
+        System.out.println();
+    }
+}
