fc10ca579144d933d2c43c982a42fed421ab1302
RICHIER Marcus <marcus.richier8@etu.univ-lorraine.fr>
2021-04-05 11:57:53 +0200

    Ajout de JoueurDebutant

--- src/jeu/JoueurDebutant.java
+++ src/jeu/JoueurDebutant.java
@@ -1,4 +1,47 @@
 package jeu;
 
-public class JoueurDebutant {
+import exceptions.*;
+
+import java.util.List;
+
+public class JoueurDebutant extends Joueur {
+
+    //contructeurs
+
+    public JoueurDebutant(){
+        super();
+    }
+
+    //methodes
+
+
+    public void calculerScore(){
+        double score = 0;
+        List<Partie> parties= this.getParties();
+        for (int i = 0; i < parties.size(); i++){
+            score += parties.get(i).getScore() * 0.25;
+        }
+        this.setScore(score);
+    }
+
+    public void ajouterPiece(int num, int x, int y) {
+        try {
+            this.getLastPartie().ajouterPiece(num, x, y);
+        } catch (PieceDebordeTerrain pieceDebordeTerrain) {
+            //autorise
+            pieceDebordeTerrain.printStackTrace();
+        } catch (CaseDejaOccupe caseDejaOccupe) {
+            //autorise
+            caseDejaOccupe.printStackTrace();
+        } catch (NumeroInconnue numeroInconnue) {
+            numeroInconnue.printStackTrace();
+        } catch (PartieInconnue partieInconnue) {
+            partieInconnue.printStackTrace();
+        } catch (CoordonneeInvalide coordonneeInvalide) {
+            coordonneeInvalide.printStackTrace();
+        }
+
+    }
+
+
 }
