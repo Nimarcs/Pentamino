ea520656d03f580876581a48e157dfedb228692c
BERGERAT Hugo <hugo.bergerat2002@gmail.com>
2021-04-08 13:57:12 +0200

    Création d'une partie lors de la création d'un joueur.

--- src/commandes/jeu/AjouterJoueurCommande.java
+++ src/commandes/jeu/AjouterJoueurCommande.java
@@ -1,6 +1,11 @@
 package commandes.jeu;
 
+import exceptions.CharInvalide;
+import exceptions.CoordonneeInvalide;
+import exceptions.ValeurNonTraite;
 import jeu.*;
+
+import java.io.IOException;
 
 public class AjouterJoueurCommande extends CommandeJeu {
 
@@ -10,16 +15,23 @@ public class AjouterJoueurCommande extends CommandeJeu {
 
     @Override
     public void afficherAide() {
-        System.out.println(" -ajouter_joueur <prenom> <debutant|intermediaire|avance>: Permet d'ajouter un joueur au jeu.");
+        System.out.println(" -ajouter_joueur <prenom> <debutant|intermediaire|avance> <tailleX> <tailleY>: Permet d'ajouter un joueur au jeu et crée une partie de taille x,Y.");
     }
 
     @Override
     public void executer(String[] args, Jeu jeu) {
-        if(args.length < 3) {
+        if(args.length < 5) {
             super.erreur("Pas assez d'arguments");
             return;
         }
         String prenom = args[1];
+        int x = 0, y = 0;
+        try {
+            x = Integer.parseInt(args[3]);
+            y = Integer.parseInt(args[4]);
+        } catch (NumberFormatException ignored) {
+            super.erreur("Mauvais format pour les arguments, tailleX et tailleY doivent être des entiers.");
+        }
         Joueur joueur = null;
         switch (args[2]) {
             case "debutant": joueur = new JoueurDebutant(prenom); break;
@@ -27,6 +39,16 @@ public class AjouterJoueurCommande extends CommandeJeu {
             case "avance": joueur = new JoueurAvance(prenom); break;
             default: this.erreur(args[2] + " n'est pas un mode de jeu reconnue.");
         }
-        if(joueur != null) jeu.getJoueurs().add(joueur);
+        if(joueur != null) {
+            try {
+                joueur.creerPartie(x, y);
+                jeu.getJoueurs().add(joueur);
+                super.info("Création du joueur réussi.");
+            } catch (CharInvalide | IOException | ValeurNonTraite charInvalide) {
+                super.erreur("Echec de la création de la partie en interne (pas censé arriver)");
+            } catch (CoordonneeInvalide coordonneeInvalide) {
+                super.erreur("TailleX et tailleY doivent être supérieur à 0.");
+            }
+        }
     }
 }
