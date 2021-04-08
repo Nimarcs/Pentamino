27f4fae506a7ad80a3f6fa3e66af4130ff891af6
BERGERAT Hugo <hugo.bergerat2002@gmail.com>
2021-04-08 13:57:52 +0200

    Suppresion de la 'exception DimensionsInvalide car inutile (CoordonneInvalide)

--- src/exceptions/DimensionsInvalide.java
+++
@@ -1,9 +0,0 @@
-package exceptions;
-
-public class DimensionsInvalide extends Exception {
-
-    public DimensionsInvalide(int x, int y) {
-        super(String.format("Dimensions (%s;%s) invalides (< 0)", x, y));
-    }
-
-}
