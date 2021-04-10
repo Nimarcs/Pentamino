11b154e53e03695ef973452a5057d9b31ae11922
RICHIER Marcus <marcus.richier8@etu.univ-lorraine.fr>
2021-04-10 13:27:05 +0200

    fix décalage lorsque plus de 10 joueur
    fix du formatage lorsque +10 joueur

--- src/commandes/jeu/AfficherJoueurCommande.java
+++ src/commandes/jeu/AfficherJoueurCommande.java
@@ -25,7 +25,11 @@ public class AfficherJoueurCommande extends CommandeJeu {
             Collections.sort(joueurs);
             System.out.println("Joueurs par score moyen:");
             for (int i = 0; i < joueurs.size(); i++) {
-                System.out.printf(" %d: %s%n", i, joueurs.get(i).toString());
+                if (i< 10) {
+                    System.out.printf(" %d: %s%n", i, joueurs.get(i).toString());
+                }else{
+                    System.out.printf(" %d:%s%n", i, joueurs.get(i).toString());
+                }
             }
         } else {
             Collections.sort(joueurs, new Comparator<Joueur>() {
@@ -35,7 +39,11 @@ public class AfficherJoueurCommande extends CommandeJeu {
             });
             System.out.println("Joueurs par ordre alphabétique:");
             for (int i = 0; i < joueurs.size(); i++) {
-                System.out.printf(" %d: %s%n", i, joueurs.get(i).toString());
+                if (i< 10) {
+                    System.out.printf(" %d: %s%n", i, joueurs.get(i).toString());
+                }else{
+                    System.out.printf(" %d:%s%n", i, joueurs.get(i).toString());
+                }
             }
         }
         jeu.setJoueurs(joueurs);
